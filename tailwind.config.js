50f14ad63c3d2e83e7bb393422acce67e48505db
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-08 14:07:38 +0000

    Add the rest

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                nutmeg: "hsl(14, 45%, 36%)",
+                darkRaspberry: "hsl(332, 51%, 32%)",
+                roseWhite: 'hsl(330, 100%, 98%)',
+                eggshell: "hsl(30, 54%, 90%)",
+                lightGrey: "hsl(30, 18%, 87%)",
+                wengeBrown: "hsl(30, 10%, 34%)",
+                darkCharcoal: "hsl(24, 5%, 18%)",
+            },
+            fontFamily: {
+                outfit: ["Outfit", "sans-serif"],
+                youngSerif: ["Young Serif", "serif"]
+            }
+        }
+    }
+}
+
